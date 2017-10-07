617cf7f67015a898392e67f635d51cb5a87f614e
Eugene Slesarev <301gene@gmail.com>
2017-10-07 00:59:55 -0400

    Part 1 of the minesweeper finished

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,11 @@
-const blankline = '  |   |  ';
+const blankLine = '  |   |  ';
 console.log(`This is what an empty board would look like:
-${blankline}
-${blankline}
-${blankline}`);
+${blankLine}
+${blankLine}
+${blankLine}`);
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+console.log(`This is what a board with a guess and a bomb on it would look like:
+${guessLine}
+${bombLine}
+${blankLine}`);
